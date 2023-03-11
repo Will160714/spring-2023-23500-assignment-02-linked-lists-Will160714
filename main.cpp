0f7a65e8c805e393acee51fc729aebe1dd68b487
Will160714 <95445527+Will160714@users.noreply.github.com>
2023-03-11 18:14:59 -0500

    Changed to main for OList

--- main.cpp
+++ main.cpp
@@ -1,33 +1,48 @@
-#include "List.h"
-#include "Node.h"
+#include "OList.h"
 
 int main(){
-    List *test = new List();
-    test -> insert("one");
-    std::cout << test -> toString() << std::endl;
-    test -> insert("two");
-    test -> insert("three");
-    std::cout << test -> toString() << std::endl;
-    std::cout << "Length: " << test -> length() << std::endl;
-    
-    //Get Function not Working
-    //std::cout << "First: " << test -> get(0) << std::endl;
-    //std::cout << "Second: " << test -> get(1) << std::endl;
-    
+    //Constructor
+    std::cout << "Constructor" << std::endl;
+    OList *test = new OList();
     std::cout << std::endl;
 
-    List *test2 = new List();
-    test2 -> insert(0, "a");
-    test2 -> insert(1, "back");
-    std::cout << test2 -> toString() << std::endl;
-    test2 -> remove(1);
-    std::cout << test2 -> toString() << std::endl;
+    //Insert
+    std::cout << "Insert" << std::endl;
+    test -> insert("1");
+    test -> insert("2");
+    test -> insert("3");
+    test -> insert("4");
+    std::cout << std::endl;
 
-    if(test2 -> contains("a"))
-        std::cout << "It is in the List" << std::endl;
-    else
-        std::cout << "Not in List" << std::endl;
+    //toString
+    std::cout << "toString" << std::endl;
+    std::cout << test -> toString() << std::endl;
+    std::cout << std::endl;
+    
+    //Contains
+    std::cout << "Contains" << std::endl;
+    if(test -> contains("1"))
+        std::cout << "1 is in OList" << std::endl;
+    if(!(test -> contains("0")))
+        std::cout << "o is NOT in OList" << std::endl;
+    std::cout << std::endl;
 
+    //Get
+    std::cout << "Get" << std::endl;
+    std::cout << "Element at Index 0 is " << test -> get(0) << std::endl;
+    std::cout << "Element at Index 4 is " << test -> get(4) << std::endl;
+    std::cout << std::endl;
 
+    //Remove
+    std::cout << "Remove" << std::endl;
+    test -> remove(2);
+    std::cout << test -> toString() << std::endl;
+    std::cout << std::endl;
+
+    //Reverse
+    std::cout << "Reverse" << std::endl;
+    test -> reverse();
+    std::cout << test -> toString() << std::endl;
     return 0;
 }
+
